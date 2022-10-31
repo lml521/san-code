641a656338b5b92da40049ebc78b4b90a552d25d
lml <2084387897@qq.com>
2022-10-31 16:07:45 +0800

    扫码功能实现

---
+++ components/code/code.js
@@ -0,0 +1,27 @@
+// components/code/code.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 子传父 当点击扫描商品按钮 传递到父组件
+    handelScanCode(){
+      this.triggerEvent('handelScanCode')
+    }
+
+  }
+})
--- pages/shopping/shopping.js
+++ pages/shopping/shopping.js
@@ -5,7 +5,39 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 轮播图数据
+    advertList:[],
+  },
+  // 获取轮播图数据
+  getAdvertList(){
+    // 模拟轮播图数据
+    const data=[
+      {
+        id:1,
+        imgUrl:"https://img1.baidu.com/it/u=2784910973,3857623495&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=375"
+      },{
+        id:2,
+        imgUrl:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F87ea1b2a1e4f42ed03c02e337b16d09416d6039e21bd7-BFN8hx_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1669780070&t=a5f4129cc105be60254ac4a7fcd8154f"
+      },{
+        id:3,
+        imgUrl:"https://img0.baidu.com/it/u=1745076922,209989677&fm=253&fmt=auto&app=138&f=JPEG?w=1200&h=375"
+      },
+    ]
+    // 小程序中数据不是响应式的 使用 this.setData 修改数据 
+    this.setData({
+      advertList:data
+    })
+  
+  },
 
+  // 点击扫描商品事件
+  handelScanCode(){
+    wx.scanCode({
+      onlyFromCamera: true,
+      success (res) {
+        console.log(res)
+      }
+    })
   },
 
   /**
@@ -19,7 +51,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    this.getAdvertList()
   },
 
   /**
